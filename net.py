ed23a558efed4c2def323cecfa5d3a65ca16a033
Reoien Ng <zhongyuanw@cqu.edu.cn>
2019-09-03 21:48:27 +0800

    Update net.py
    add log_softmax

--- net.py
+++ net.py
@@ -109,8 +109,8 @@ class DenseNet(nn.Module):
         out = self.dense3(out)
         out = torch.squeeze(F.avg_pool2d(F.relu(self.bn1(out)), 8))
 
-        out = self.fc(out)
-        # out = F.log_softmax(self.fc(out),dim=0)
+        # out = self.fc(out)
+        out = F.log_softmax(self.fc(out),dim=0)
         return out
 
 
