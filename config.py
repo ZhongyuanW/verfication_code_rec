198b3670de948d2f77d25dcc39607d74b9c5d6d9
Reoien Ng <zhongyuanw@cqu.edu.cn>
2019-10-10 10:12:21 +0000

    reduce batch size, adjust lr

--- config.py
+++ config.py
@@ -6,7 +6,7 @@
 # @Software: PyCharm
 
 HOME = "/home/zhongyuan/datasets/VerifiCodeRef"
-LR_STEP = (16000,30000)
+LR_STEP = (15000,30000)
 CLASS = [
             ' ',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -21,7 +21,7 @@ CLASS = [
 
 CLASS_NUM = 62
 
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 MAX_SIZE = 40000
 LR = 1e-1
 MOMENTUM = 0.9
